b1c159409d5e8d8b9f255e2ceb49824e3ab9b24f
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-02-04 19:19:06 +0900

    1.2 Getting the Location

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Loading from './Loading';
+import * as Location from'expo-locaiton';
 
-export default function App() {
-  return (
+export default class extends React.Component {
+  getLocation=async function(){
+    const location=await Location.getCurrentPositionAsync();
+    console.log(location);
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render() {
+    return(
     <Loading>
-      
-    </Loading>
-  );
+
+    </Loading>);
+  };
 }
